0194f007d3bc1ce8a9c20bf4e8bcfef99d9294e4
Oded Arbel <oded@geek.co.il>
2022-09-06 18:26:26 +0300

    Registration & call reception works

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # cloudonix-sample-reg-free-server
-An example NodeJS server to implement the Cloudonix Registration-Free protocol and support iOS and Android clients
+
+An example NodeJS server to implement the Cloudonix Registration-Free protocol and support iOS and Android clients.
+
+We use [`node-pushnotifications`](https://www.npmjs.com/package/node-pushnotifications) in this example to make it
+easier to support all kinds of target devices, but this project is currently only configured and tested for Android
+devices. Work to support iOS devices will be added soon.
+
+# Setup
+
+## Initial Setup
+
+1. Clone this repository.
+1. Run `npm install`.
+1. Make sure you have `ngrok` installed - we use it to publish out testing server from our local machine so the
+   Cloudonix Platform can call it. If you have some other way to publish the server (for example, by running
+   the service on a publicly accessible server), you can ignore the ngrok instructions and set up your own.
+
+## Create Firebase Cloud Messaging
+
+1. Go to the [Firebase website](https://firebase.google.com/) and click "Go to console". If you are not logged in to
+   your Google account, you'd be asked to log in at this point.
+1. Click "Add project", and specify the project name. It may be a good idea to "select parent resource" as well if you
+   are putting this server into an organization. Continue and create the project.
+1. After the project is created, click the gear icon in the sidebar and go into "Project settings", then into then
+   "Cloud Messaging" tab, and click "Manage Service Accounts" which will open Google Cloud IAM console in a new page.
+1. In the IAM console "Service Accounts" view, there should already be a service account that Firebase created for you,
+   so click on the three dots menu for that account, then select "Manage Keys" - we need an API key to authenticate
+   the sample server to Firebase - this is the simplest way to authenticate, though Google recommends you use a
+   "workload identity" instead - there would be a link in that page to read more about having better security when
+   running in the cloud.
+1. Click "Add Key" then "Create new key" and "Create" - which will download a service account key file named
+   with your Firebase project name followed by a random string and a `.json` extension. Move that file to the project
+   working directory and name it `firebase-service-account-key.json` (this file is git ignored, so it will not be
+   published).
+
+## Run The Service
+
+1. Run `npm run-script run`. This would start the server and will wait for registrations and incoming calls.
+1. In another terminal, run `npm run-script ngrok`. This would start the ngrok proxy that will forward requests from the
+   Cloudonix Platform to the testing service running on your workstation. Record the "Forwarding" HTTPS URL that ngrok
+   shows.
+1. Log in to the [Cloudonix Cockpit](https://cockpit.cloudonix.io), go into the domain you want to set up to send
+   Registration-Free call from, and go into its settings, then to the tab "Domain Settings".
+1. Set the URL from ngrok in the "Registration-Free Control Endpoint URL" field, add "`/incoming`" to the end of it
+   and click the check mark to apply (e.g. `https://your-server-id.ngrok.io/incoming`).
+
+## Perform A Test Call
+
+1. Set up a mobile application that uses the Cloudonix Mobile SDK (See the [Cloudonix Sample Apps repository](https://github.com/CloudonixMobileSDK-SampleApps)
+   if you want to use a ready-made sample app to test), and set it to register push notification and MSISDN using
+   the URL you got from ngrok in the previous step, with `/devices` added at the end (e.g.
+   `https://your-server-id.ngrok.io/devices`) - see the source code in this repository for the exact details on how to
+   format the registration request from the device.
+2. Run the application and monitor the example service logs to see that it registered correctly.
+3. Make a call into your Cloudonix domain, to the MSISDN registered in the example service (for example, by setting up
+   a SIP soft phone to connect to your Cloudonix domain, see the Cloudonix documentation for instructions).
+
+You should now see an incoming call in the example server log, and your app should receive the incoming call
+notification.
---
+++ src/index.js
@@ -0,0 +1,166 @@
+#!/usr/bin/env node 
+'use strict';
+
+const fcmAccount = require('../firebase-service-account-key.json');
+const http = require('http');
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const firebaseadmin = require("firebase-admin");
+
+class Database {
+	db = new sqlite3.Database(':memory:');
+	
+	async run(sql, params) {
+		return new Promise((res,rej) => {
+			function callback(err) { if (err) rej(err); else res(this.lastID); }
+			this.db.run(sql, params, callback);
+		});
+	}
+	
+	async all(sql, params) {
+		return new Promise((res,rej) => {
+			function callback(err, rows) { if (err) rej(err); else res(rows); }
+			this.db.all(sql, params, callback);
+		});
+	}
+	
+	async init() {
+		return this.run(`CREATE TABLE IF NOT EXISTS devices (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			os_type VARCHAR(7),
+			identifier VARCHAR(40) UNIQUE,
+			msisdn VARCHAR(15) UNIQUE
+		);`, []);
+	}
+	
+	async listDevices() {
+		return this.all("SELECT * FROM devices", []);
+	}
+	
+	async registerDevice(msisdn, identifier, ostype) {
+		try {
+			let lastid = await this.run('INSERT INTO devices (os_type, identifier, msisdn) VALUES (?, ?, ?)', [ ostype, identifier, msisdn ]);
+			console.log(`New device ${ostype}:${identifier} registered MSISDN ${msisdn} => ${lastid}`);
+			let device = await this.getDevice(identifier);
+			if (!device)
+				throw new Error("INSERT did not return any row!");
+			return device;
+		} catch (err) {
+			if (err.code != 'SQLITE_CONSTRAINT')
+				throw err;
+			if (err.message.includes('devices.identifier'))
+				return this.updateMSISDN(identifier, msisdn);
+			if (err.message.includes('devices.msisdn')) {
+				await this.deleteByMSISDN(msisdn);
+				return this.registerDevice(msisdn, identifier, ostype);
+			}
+			throw err;
+		}
+	}
+	
+	async getDevice(identifier) {
+		let rows = await this.all('SELECT * FROM devices WHERE identifier = ?', [ identifier ]);
+		if (rows.length)
+			return rows[0];
+		return null;
+	}
+	
+	async getDeviceByMSISDN(msisdn) {
+		let rows = await this.all('SELECT * FROM devices WHERE msisdn = ?', [ msisdn ]);
+		if (rows.length)
+			return rows[0];
+		return null;
+	}
+	
+	async updateMSISDN(identifier, msisdn) {
+		await this.run('UPDATE devices SET msisdn = ? WHERE identifier = ?', [ msisdn, identifier]);
+		console.log(`Updated device ${identifier} with new MSISDN ${msisdn}`);
+		return this.getDevice(identifier);
+	}
+	
+	async deleteDevice(identifier) {
+		console.log('Removing device', identifier);
+		return this.run('DELETE FROM devices WHERE identifier = ?', [ identifier ]);
+	}
+	
+	async deleteByMSISDN(msisdn) {
+		console.log(`Removing all devices with msisdn ${msisdn}`);
+		return this.run('DELETE FROM devices WHERE msisdn = ?', [ msisdn ]);
+	}
+}
+
+const app = express();
+const db = new Database();
+const push = firebaseadmin.initializeApp({
+	credential: firebaseadmin.credential.cert(fcmAccount),
+	projectId: 'cloudonix-sample-reg-free'
+}).messaging();
+
+app.use(express.json());
+
+/**
+ * Healthcheck responder
+ */
+app.get('/', (req, res, next) => {
+	res.send({status:true});
+});
+
+/**
+ * Convenience helper to list all device identifiers.
+ * This is for testing and inherently insecure - do not put in production
+ */
+app.get('/devices', async (req, res) => {
+	let devices = await db.listDevices();
+	res.send(devices);
+});
+
+/**
+ * Register a new device by sending a JSON as such: {"identifier": "your-app-PN-identifier", "msisdn": "number-you-want-to-use", "type": "android|ios"}
+ */
+app.post('/devices', async (req, res) => {
+	if (!req.body || !req.body.identifier)
+		return res.status(400).send({status:false, message:"Missing device 'identifier'"});
+	if (!req.body.msisdn)
+		return res.status(400).send({status:false, message:"Missing device 'msisdn'"});
+ 	let device = await db.registerDevice(req.body.msisdn, req.body.identifier, req.body.type || 'android');
+	res.send(device);
+});
+
+/**
+ * Handle the Cloudonix Platform incoming call message
+ */
+app.post('/incoming', async (req, res) => {
+	let body = req.body;
+	if (!body || !body.session)
+		return res.status(400).send({status: false, message: 'Not a valid Cloudonix registration-free message!'});
+	let device = await db.getDeviceByMSISDN(body.dnid);
+	if (!device)
+		return res.status(404).send({status: false, message: `Subscriber ${body.dnid} could not be found`});
+	let notification = { 
+		session: body.session,
+		callerId: body['caller-id'],
+		ringingURL: `${body.endpoint}/calls/${body.domain}/ringing/${body.subscriber.msisdn}/${body.session}`,
+	};
+	console.log('Sending push notification to', device, 'with message', notification);
+	let pushResult = await push.sendToDevice(device.identifier, {
+		data: notification,
+	});
+	console.log("Push result: %j", pushResult);
+	if (pushResult.failureCount > 0) {
+		let error = (((pushResult.results || [])[0] || {}).error || {}).code || 'unknown-error';
+		if (error == 'messaging/registration-token-not-registered')
+			db.deleteDevice(device.identifier);
+		res.status(500).send({
+			status: false,
+			message: `Failed to send push notification due to ${error}`
+		});
+	} else {
+		res.status(201).send({status: true, message: 'Sent push notification'});
+	}
+});
+
+db.init().then(_ => {
+	console.log("Database setup complete");
+	app.listen(8780);
+	console.log("Server is listening on http://localhost:8780/");
+});
